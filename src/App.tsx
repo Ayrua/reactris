cabefb0250e22baeaaa150fae586603f6f10651f
Alan <20212970+Ayrua@users.noreply.github.com>
2024-04-14 17:20:10 +0200

    add: basic grid view

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import Board from "./component/Board"
+
 const App = () => {
+  const board = new Array(20).fill(0).map(() => new Array(10).fill(0));
+  console.log(board)
+
+  board[10][5] = 1
+  board[11][5] = 1
+  board[12][5] = 1
+  board[11][4] = 1
+
   return (
-    <></>
+    <div>
+      <Board board={board} />
+    </div>
   )
 }
 
---
+++ src/component/Board.tsx
@@ -0,0 +1,34 @@
+
+interface Props {
+    board: number[][]
+}
+
+interface CellProp {
+    type: number
+}
+
+const Cell = ({ type }: CellProp) => {
+    return (
+        <div className={`t_${type}`}></div>
+    )
+}
+
+const Board = ({ board }: Props) => {
+    // console.log(board)
+
+    return (
+        <div className="board">
+            {board.map((row, rowId) => (
+                <div className={'row'} key={rowId}>
+                    {
+                        row.map((cell, cellId) => (
+                            <Cell type={cell} key={`${rowId},${cellId}`} />
+                        ))
+                    }
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Board
