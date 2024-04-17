d49f7ed5e35a039ee6a1f866127bad7ea79b4692
Alan <20212970+Ayrua@users.noreply.github.com>
2024-04-17 12:08:21 +0200

    add: basic game loop

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
+import { useEffect, useRef, useState } from "react";
 import Board from "./component/Board"
-import { drawBoard, setShapeOnBoard } from "./utils/utils";
+import { drawBoard } from "./utils/utils";
+import { GameLoop } from "./utils/GameLoop";
 
 const App = () => {
   const board = new Array(20).fill(0).map(() => new Array(10).fill(0));
+  const [newBoard, setNewBoard] = useState(drawBoard(1, board[0].length / 2, 0, 0, board))
+  const piece = useRef([1, board[0].length / 2, 0, 0])
 
-  const newBoard = drawBoard(1, board[0].length / 2, 0, 0, board)
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      GameLoop(piece.current, board)
+      setNewBoard(drawBoard(piece.current[0], piece.current[1], piece.current[2], piece.current[3], board))
+    }, 1000);
 
-  // check reference and drawing is not the same 
-  console.log(board[0][7] != newBoard[0][7]) // should be true
-  setShapeOnBoard(1, board[0].length / 2, 0, 0, board)
-  const newBoard2 = drawBoard(1, board[0].length / 2, 0, 0, board)
-  console.log(board[0][7] != newBoard2[0][7]) // should be false
+    return () => clearInterval(intervalID);
+  }, []);
 
   return (
     <div>
---
+++ src/utils/GameLoop.ts
@@ -0,0 +1,5 @@
+
+
+export const GameLoop = (piece: number[], board: number[][]) => {
+    piece[2] = piece[2] + 1
+}
