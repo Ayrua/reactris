29c03604af9427af20808c9a465895cfebf60f9b
Alan <20212970+Ayrua@users.noreply.github.com>
2024-04-23 14:20:40 +0200

    refactoring App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,10 @@
-import { useEffect, useRef, useState } from "react";
-import Board from "./component/Board"
-import { drawBoard, move } from "./utils/utils";
-import { GameLoop } from "./utils/GameLoop";
+import Reactris from "./component/Reactris";
 
 const App = () => {
-  const board = new Array(24).fill(0).map(() => new Array(10).fill(0));
-  const piece = useRef([2, board[0].length / 2, 0, 0])
-  const [idx, x, y, r] = piece.current
-  const [newBoard, setNewBoard] = useState(drawBoard(idx, x, y, r, board))
-  const points = useRef(0)
-  const gameOver = useRef(false)
-  const level = (Math.floor(points.current / 300)) + 3
-  const fps = 1000 / level
-
-  useEffect(() => {
-    document.addEventListener('keydown', keyChange, true)
-    setInterval(() => {
-      const [idx, x, y, r] = piece.current
-      setNewBoard(drawBoard(idx, x, y, r, board))
-    }, 1);
-  }, [])
-
-  const keyChange = (e: any) => {
-    if (!gameOver.current) {
-      move(piece.current, board, e.key)
-    }
-  }
-
-  useEffect(() => {
-
-    const intervalID = setInterval(() => {
-      if (!gameOver.current) {
-        const [g, p] = GameLoop(piece.current, board, points.current)
-        gameOver.current = g
-        points.current = p
-      }
-    }, fps);
-
-
-    return () => clearInterval(intervalID);
-  }, [gameOver.current]);
 
   return (
     <div>
-      <Board board={newBoard} />
-      <div className="gameovertext">{gameOver.current ? 'game over' : ''}</div>
-      <div className="infotext">points: {points.current} | level:{level - 2} </div>
-      <div className="infotext">controls: wasd | arrowkeys | r</div>
+      <Reactris />
     </div>
   )
 }
